e2a33d3410934c2425b7dd09c61c259d2ab50384
Emir Hasanbegovic <emir@emirweb.com>
2014-04-26 21:43:35 -0400

    @emir-hasanbegovic completed UI, added icons

--- src/com/sample/glass/glasssample/GlassActivity.java
+++ src/com/sample/glass/glasssample/GlassActivity.java
@@ -3,13 +3,16 @@ package com.sample.glass.glasssample;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardScrollView;
 import com.sample.glass.glasssample.adapters.ParkingAdapter;
 import com.sample.glass.glasssample.model.Parking;
@@ -33,6 +36,7 @@ public class GlassActivity extends Activity {
 	private Handler mHandler;
 	private TextView mProgressTextView;
 	private View mProgressContainer;
+	private View mResultsContainer;
 	private CardScrollView mCardScrollView;
 	private LocationHelper mLocationHelper;
 	private final Runnable mTimeout = new Runnable() {
@@ -61,6 +65,8 @@ public class GlassActivity extends Activity {
 		setContentView(R.layout.activity_glass);
 		mProgressTextView = (TextView) findViewById(R.id.activity_glass_progress_text);
 		mProgressContainer = findViewById(R.id.activity_glass_progress_container);
+		mResultsContainer = findViewById(R.id.activity_glass_results_container);
+		
 		mCardScrollView = (CardScrollView) findViewById(R.id.activity_glass_results);
 
 		mLocationHelper = new LocationHelper(this);
@@ -109,16 +115,19 @@ public class GlassActivity extends Activity {
 		if (foundParking) {
 			mFindingParking = false;
 			mProgressContainer.setVisibility(View.GONE);
-			mCardScrollView.setVisibility(View.VISIBLE);
+			mResultsContainer.setVisibility(View.VISIBLE);
 			final ParkingAdapter parkingAdapter = new ParkingAdapter(getApplicationContext());
 			parkingAdapter.setParkingList(parkingList);
 			mCardScrollView.setAdapter(parkingAdapter);
 			mCardScrollView.activate();
+			final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+			audio.playSoundEffect(Sounds.SUCCESS);
 			return;
 		}
+		
 
 		mProgressContainer.setVisibility(View.VISIBLE);
-		mCardScrollView.setVisibility(View.GONE);
+		mResultsContainer.setVisibility(View.GONE);
 		final boolean hasLocation = mLocation != null;
 		final boolean hasParkingLists = ParkingApplication.LAWN_PARKING_LIST != null && ParkingApplication.GREEN_PARKING_LIST != null;
 		if (!hasParkingLists) {
